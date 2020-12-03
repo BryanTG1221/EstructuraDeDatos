1f032fec10057d626f3070bf20515d2b0b6eeb84
BryanTG1221 <61299735+BryanTG1221@users.noreply.github.com>
2020-12-02 18:47:46 -0600

    ACTUALIZACION metodos de ordenamiento: Bubble Sort 100% Quick Sort 100% Shaker Sort 100% Shell Sort 100% Radix Sort 0%

---
+++ Programas/Algoritmos de Ordenamiento/Shell Sort.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metodo_Ordenamiento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+         Random RandomInt = new Random();
+        Algoritmos Sorteadores = new Algoritmos();
+        static int Tamaño;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tamaño = Convert.ToInt32(textBox1.Text);
+            int[] Vector = new int[Tamaño];
+            Random randomLOL = new Random();
+
+            for (int i = 0; i < Vector.Length; i++)
+            {
+                Vector[i] = randomLOL.Next(0, 100);
+            }
+
+            for (int i = 0; i < Tamaño; i++)
+            {
+                MessageBox.Show("Espacio " + i + " : " + Vector[i]);
+            }
+            
+
+            shellSort(Vector,Tamaño);
+            //-----------------------------------------------------------------------------------
+            //------------------------------MOSTRAR ARREGLO ORDENADO-----------------------------
+            MessageBox.Show("Se sorteo el arreglo");
+            for (int i = 0; i < Tamaño; i++)
+            {
+                MessageBox.Show("Espacio " + i + ": " + Vector[i]);
+            }
+
+        }
+
+        //-----------------------------------------------------------------------------------------
+        //----------------------------------SHELL SORT-------------------------------------------
+        //----------------------------------------------------------------------------------
+
+        static void shellSort(int[] arr, int array_size)
+        {
+            int i, j, inc, temp;
+            inc = 3;
+            while (inc > 0)
+            {
+                for (i = 0; i < array_size; i++)
+                {
+                    j = i;
+                    temp = arr[i];
+                    while ((j >= inc) && (arr[j - inc] > temp))
+                    {
+                        arr[j] = arr[j - inc];
+                        j = j - inc;
+                    }
+                    arr[j] = temp;
+                }
+                if (inc / 2 != 0)
+                    inc = inc / 2;
+                else if (inc == 1)
+                    inc = 0;
+                else
+                    inc = 1;
+            }
+        }
+
+        //-----------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------
+
+    }
+}
